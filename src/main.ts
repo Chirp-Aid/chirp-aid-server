e425e89ddd7ef74fe6abaa01f22788b7067bc43a
Journeybongbong <77794756+Journeybongbong@users.noreply.github.com>
2023-07-17 20:44:48 +0900

    feat: add Swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from "dotenv"
 import * as path from "path"
+import { BaseAPIDocument } from './swagger.document';
+import { SwaggerModule } from '@nestjs/swagger';
 
 
 dotenv.config({
@@ -10,6 +12,10 @@ dotenv.config({
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swaggerConfig = new BaseAPIDocument().initializeOptions();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/swagger.document.ts
@@ -0,0 +1,21 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export class BaseAPIDocument {
+    public builder = new DocumentBuilder();
+    public initializeOptions() {
+    return this.builder
+        .setTitle('Chirp-Aid(칩에이드)')
+        .setDescription('Chirp-Aid Swagger API 서버')
+        .setVersion('1.0.0')
+        // .addBearerAuth(
+        //     {
+        //         type: 'http'
+        //         scheme: ''
+        //         name: 'JWT'
+        //         in: 'header'
+        //     },
+        //     access-token',
+        // )
+        .build();
+    }
+}
