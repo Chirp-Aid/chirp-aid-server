2a351420d8872aff722673f89ec1240b05173dd1
Journeybongbong <77794756+Journeybongbong@users.noreply.github.com>
2023-09-20 21:23:29 +0900

    Docs: 장바구니Swagger추가

--- src/donate/donate.controller.ts
+++ src/donate/donate.controller.ts
@@ -3,7 +3,9 @@ import { DonateService } from './donate.service';
 import { AddBasektDto } from './dto/add-basket.dto';
 import { BasketService } from './basket.service';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiHeader, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('DONATE: 기부 관련 요청')
 @Controller('donate')
 export class DonateController {
   constructor(
@@ -11,6 +13,33 @@ export class DonateController {
     ) {}
 
   @Post('basket')
+  @ApiOperation({
+    summary: '장바구니 추가',
+    description:
+      '보육원의 요청 물품을 사용자의 장바구니에 추가합니다.',
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer {Access Token}',
+    example: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Created',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unaothorized',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Not Found - 해당 요청을 찾을 수 없습니다.\
+    \nNot Found - 해당 사용자를 찾을 수 없습니다. (사용자 계정이 아닌 보육원 계정인 경우)',
+  })
+  @ApiResponse({
+    status: 409,
+    description: '해당 물품은 이미 장바구니에 있습니다.',
+  })
   @UseGuards(AuthGuard('access'))
   async addBasekt(@Body() addBasektDto: AddBasektDto, @Request() req)
   {
@@ -19,6 +48,28 @@ export class DonateController {
   }
 
   @Get('basket')
+  @ApiOperation({
+    summary: '장바구니 조회',
+    description:
+      '사용자의 장바구니를 조회합니다.',
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer {Access Token}',
+    example: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Created',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unaothorized',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Not Found - 해당 사용자를 찾을 수 없습니다. (사용자 계정이 아닌 보육원 계정인 경우)',
+  })
   @UseGuards(AuthGuard('access'))
   async getBaskets(@Request() req)
   {
