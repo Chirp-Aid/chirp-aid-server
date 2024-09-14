fbd31108f02473629193a71a8b1ddab0dc3018b7
juyeok <kyoung1678@naver.com>
2024-09-14 21:25:37 +0900

    ✨ 관리자 페이지 :보육원 생성 + 수정 기능 추가

--- src/manager/manager.controller.ts
+++ src/manager/manager.controller.ts
@@ -1,8 +1,10 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
   Param,
+  Patch,
   Post,
   UseGuards,
 } from '@nestjs/common';
@@ -11,11 +13,19 @@ import { RolesGuard } from 'src/commons/guards/roles.guard';
 import { Roles } from 'src/commons/decorators/roles.decorator';
 import { AuthGuard } from '@nestjs/passport';
 import { Orphanage } from 'src/entities/orphanage.entity';
+import { CreateOrphanageDto } from './dto/create-orphanage.dto';
+import { UpdateOrphanageDto } from 'src/orphanages/dto/updateOrphanage.dto';
 
 @UseGuards(AuthGuard('access'), RolesGuard)
 @Controller('manager')
 export class ManagerController {
   constructor(private readonly managerService: ManagerService) {}
+  @Post('orphanage')
+  @Roles('admin')
+  async createOrphanage(@Body() createOrphanageDto: CreateOrphanageDto) {
+    return await this.managerService.createOrphanage(createOrphanageDto);
+  }
+
   @Get('orphanage')
   @Roles('admin')
   async getAllOrphanage(): Promise<Orphanage[]> {
@@ -25,6 +35,18 @@ export class ManagerController {
   @Roles('admin')
   async getOneOrphanage(@Param('id') orphanageId: number): Promise<Orphanage> {
     return await this.managerService.findOrphanageById(orphanageId);
+  }
+
+  @Patch('orphanage/:id')
+  @Roles('admin')
+  async updateOrphanage(
+    @Param('id') orphanageId: number,
+    @Body() updateOrphanageDto: UpdateOrphanageDto,
+  ) {
+    return await this.managerService.updateOrphanageById(
+      orphanageId,
+      updateOrphanageDto,
+    );
   }
 
   @Delete('orphanage/:id')
