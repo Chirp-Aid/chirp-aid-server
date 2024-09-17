d18455a727c18cdcce136586e88da83ca8a59d78
oko_jin <dswvgw6412@naver.com>
2024-09-17 14:06:01 +0900

    enh: AdminOrphanageUserModule 추가

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AdminOrphanageModule } from './orphanage/admin-orphanage.module';
 import { AdminBoardModule } from './post/admin-board.module';
 import { AdminUserModule } from './user/admin-user.module';
+import { AdminOrphanageUsersModule } from './orphanageUser/admin-orphanage-user.module';
 
 @Module({
-  imports: [AdminUserModule, AdminOrphanageModule, AdminBoardModule],
+  imports: [
+    AdminUserModule,
+    AdminOrphanageUsersModule,
+    AdminOrphanageModule,
+    AdminBoardModule,
+  ],
 })
 export class AdminModule {}
