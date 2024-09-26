1d24cf2067bd2d1fa1c9bb340139074ba059c8cb
oko_jin <dswvgw6412@naver.com>
2024-09-26 16:12:46 +0900

    feat: 채팅방 Entity 구현

---
+++ src/entities/chat-room.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from './user.entity';
+import { OrphanageUser } from './orphanage-user.entity';
+
+@Entity('chat_room')
+export class ChatRoom {
+  @PrimaryGeneratedColumn()
+  chat_room_id: string;
+
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn([{ name: 'user', referencedColumnName: 'user_id' }])
+  user: User;
+
+  @ManyToOne(() => OrphanageUser, { onDelete: 'CASCADE' })
+  @JoinColumn([
+    { name: 'orphanage_user', referencedColumnName: 'orphanage_user_id' },
+  ])
+  orphanage_user: OrphanageUser;
+}
