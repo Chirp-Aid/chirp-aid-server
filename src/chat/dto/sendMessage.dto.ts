27abaa40fb5897ca6d17a188b5444e6c89d13a64
oko_jin <dswvgw6412@naver.com>
2024-09-26 16:12:13 +0900

    feat: 메시지 전송 dto 작성

---
+++ src/chat/dto/sendMessage.dto.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { ChatRoom } from 'src/entities/chat-room.entity';
+
+export class SendMessageDto {
+  @ApiProperty({
+    example: '차캐핑',
+    description: '메시지 보내는 사람',
+    required: true,
+  })
+  sender: string;
+
+  @ApiProperty({
+    example: 'USER',
+    description: '메시지 보내는 사람 타입',
+    required: true,
+  })
+  type: 'USER' | 'ORPHANAGE_USER';
+
+  @ApiProperty({
+    example: 'room123',
+    description: '대화방 ID',
+    required: true,
+  })
+  room: ChatRoom;
+
+  @ApiProperty({
+    example: '안녕하세요',
+    description: '메시지 내용',
+    required: true,
+  })
+  content: string;
+}
