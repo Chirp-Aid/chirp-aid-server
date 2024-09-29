a024d003c9b3f8a931a15a4d14830990bc97fff5
oko_jin <dswvgw6412@naver.com>
2024-09-30 02:15:13 +0900

    feat: chat 모듈 추가

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -4,9 +4,11 @@ import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
 import { ChatRoom } from 'src/entities/chat-room.entity';
 import { Message } from 'src/entities/message.entity';
+import { User } from 'src/entities/user.entity';
+import { OrphanageUser } from 'src/entities/orphanage-user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ChatRoom, Message])], // ChatRoom과 Message 엔티티를 TypeORM 모듈에 등록
+  imports: [TypeOrmModule.forFeature([ChatRoom, Message, User, OrphanageUser])],
   providers: [ChatService, ChatGateway],
   exports: [ChatService],
 })
